e37fc63ac28e427628082413477fd09fba93605e
Zdeněk Böhm <zdenek.bohm@nic.cz>
2026-04-29 13:34:47 +0200

    Add attribute query into hideMonthYearLinks in hide-month-year-links.js.

--- aldryn_newsblog/static/aldryn_newsblog/js/src/hide-month-year-links.js
+++ aldryn_newsblog/static/aldryn_newsblog/js/src/hide-month-year-links.js
@@ -1,7 +1,7 @@
-export const hideMonthYearLinks = () => {
+export const hideMonthYearLinks = (query) => {
     for (const plugin of document.querySelectorAll('.aldryn-newsblog-month-year-links')) {
         const years = new Set()
-        for (const article of document.querySelectorAll('.djangocms-newsblog-article-list .c-article')) {
+        for (const article of document.querySelectorAll(query)) {
             years.add(article.dataset.year)
         }
         for (const link of plugin.getElementsByTagName('a')) {
