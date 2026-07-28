8ec72ca5999911f1cb3379b0a8f665ec1d90916e
Zdeněk Böhm <zdenek.bohm@nic.cz>
2026-07-28 08:22:34 +0200

    Fix resolve the page for current language in NewsBlogToolbar.enable_edit_page_content.

--- aldryn_newsblog/cms_toolbars.py
+++ aldryn_newsblog/cms_toolbars.py
@@ -79,10 +79,11 @@ class NewsBlogToolbar(CMSToolbar):
                     redirect_url = self.get_on_delete_redirect_url(obj, language=language)
                     url = get_admin_url('aldryn_newsblog_article_delete', [obj.pk])
                     menu.add_modal_item(_('Delete this article'), url=url, on_close=redirect_url)
-        try:
-            self.enable_edit_page_content(language)
-        except (ContentType.DoesNotExist, PageContent.DoesNotExist):
-            pass
+        with override(language):
+            try:
+                self.enable_edit_page_content(language)
+            except (ContentType.DoesNotExist, PageContent.DoesNotExist):
+                pass
 
     def enable_edit_page_content(self, language: str) -> None:
         """Enable edit PageContent."""
