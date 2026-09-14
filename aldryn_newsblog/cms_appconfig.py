30c22a4182a1a3d858aee4bfad0b9b5fde679c5d
Zdeněk Böhm <zdenek.bohm@nic.cz>
2026-09-14 13:42:48 +0200

    Add backward compatibility with PlaceholderField.

--- aldryn_newsblog/cms_appconfig.py
+++ aldryn_newsblog/cms_appconfig.py
@@ -3,7 +3,11 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from cms.models.fields import PlaceholderRelationField
+
+try:
+    from cms.models.fields import PlaceholderRelationField
+except ImportError:
+    from cms.models.fields import PlaceholderField as PlaceholderRelationField
 
 from aldryn_apphooks_config.models import AppHookConfig
 from aldryn_apphooks_config.utils import setup_config
